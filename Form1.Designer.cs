c57224cf0c9028f775403168c63ac52072a482ba
Антонов Антон <antonov.a.v@outlook.com>
2019-04-08 01:16:28 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace AudioVideoPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrVideo = new System.Windows.Forms.Timer(this.components);
+            this.pnlVideo = new System.Windows.Forms.Panel();
+            this.btnVolume = new System.Windows.Forms.Button();
+            this.btnFullscreen = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPlayPause = new System.Windows.Forms.Button();
+            this.lblVideo = new System.Windows.Forms.Label();
+            this.lblVideoPosition = new System.Windows.Forms.Label();
+            this.trackVolume = new System.Windows.Forms.TrackBar();
+            this.lstVideos = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlVideo
+            // 
+            this.pnlVideo.Location = new System.Drawing.Point(0, 0);
+            this.pnlVideo.Name = "pnlVideo";
+            this.pnlVideo.Size = new System.Drawing.Size(504, 339);
+            this.pnlVideo.TabIndex = 3;
+            // 
+            // btnVolume
+            // 
+            this.btnVolume.Location = new System.Drawing.Point(112, 437);
+            this.btnVolume.Name = "btnVolume";
+            this.btnVolume.Size = new System.Drawing.Size(75, 23);
+            this.btnVolume.TabIndex = 26;
+            this.btnVolume.Text = "Уровень";
+            this.btnVolume.UseVisualStyleBackColor = true;
+            this.btnVolume.Click += new System.EventHandler(this.btnVolume_Click_1);
+            // 
+            // btnFullscreen
+            // 
+            this.btnFullscreen.Location = new System.Drawing.Point(112, 466);
+            this.btnFullscreen.Name = "btnFullscreen";
+            this.btnFullscreen.Size = new System.Drawing.Size(75, 23);
+            this.btnFullscreen.TabIndex = 24;
+            this.btnFullscreen.UseVisualStyleBackColor = true;
+            this.btnFullscreen.Click += new System.EventHandler(this.btnFullscreen_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(12, 498);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(94, 23);
+            this.btnPrevious.TabIndex = 23;
+            this.btnPrevious.Text = "Предыдущее";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click_1);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(12, 468);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(94, 23);
+            this.btnNext.TabIndex = 22;
+            this.btnNext.Text = "Следующее";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click_1);
+            // 
+            // btnPlayPause
+            // 
+            this.btnPlayPause.Location = new System.Drawing.Point(12, 438);
+            this.btnPlayPause.Name = "btnPlayPause";
+            this.btnPlayPause.Size = new System.Drawing.Size(94, 23);
+            this.btnPlayPause.TabIndex = 21;
+            this.btnPlayPause.Text = "Воспроизвести";
+            this.btnPlayPause.UseVisualStyleBackColor = true;
+            this.btnPlayPause.Click += new System.EventHandler(this.btnPlayPause_Click_1);
+            // 
+            // lblVideo
+            // 
+            this.lblVideo.AutoSize = true;
+            this.lblVideo.Location = new System.Drawing.Point(193, 438);
+            this.lblVideo.Name = "lblVideo";
+            this.lblVideo.Size = new System.Drawing.Size(35, 13);
+            this.lblVideo.TabIndex = 20;
+            this.lblVideo.Text = "label1";
+            // 
+            // lblVideoPosition
+            // 
+            this.lblVideoPosition.AutoSize = true;
+            this.lblVideoPosition.Location = new System.Drawing.Point(193, 468);
+            this.lblVideoPosition.Name = "lblVideoPosition";
+            this.lblVideoPosition.Size = new System.Drawing.Size(35, 13);
+            this.lblVideoPosition.TabIndex = 27;
+            this.lblVideoPosition.Text = "label1";
+            // 
+            // trackVolume
+            // 
+            this.trackVolume.Location = new System.Drawing.Point(179, 484);
+            this.trackVolume.Name = "trackVolume";
+            this.trackVolume.Size = new System.Drawing.Size(316, 45);
+            this.trackVolume.TabIndex = 28;
+            this.trackVolume.Scroll += new System.EventHandler(this.trackVolume_Scroll);
+            // 
+            // lstVideos
+            // 
+            this.lstVideos.FormattingEnabled = true;
+            this.lstVideos.Location = new System.Drawing.Point(0, 345);
+            this.lstVideos.Name = "lstVideos";
+            this.lstVideos.Size = new System.Drawing.Size(504, 69);
+            this.lstVideos.TabIndex = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 533);
+            this.Controls.Add(this.lstVideos);
+            this.Controls.Add(this.trackVolume);
+            this.Controls.Add(this.btnVolume);
+            this.Controls.Add(this.btnFullscreen);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPlayPause);
+            this.Controls.Add(this.lblVideo);
+            this.Controls.Add(this.lblVideoPosition);
+            this.Controls.Add(this.pnlVideo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.trackVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrVideo;
+        private System.Windows.Forms.Panel pnlVideo;
+        private System.Windows.Forms.Button btnVolume;
+        private System.Windows.Forms.Button btnFullscreen;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPlayPause;
+        private System.Windows.Forms.Label lblVideo;
+        private System.Windows.Forms.Label lblVideoPosition;
+        private System.Windows.Forms.TrackBar trackVolume;
+        private System.Windows.Forms.ListBox lstVideos;
+    }
+}
+
